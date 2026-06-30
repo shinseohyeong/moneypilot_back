2ff32a1202e9b4bb5c28e46a22acdae413ead8ec
daeun <daeun3929@gmail.com>
2026-06-30 16:38:58 +0900

    feat: 보험 상품 조회 API 구현

--- app/routers/financial_product_router.py
+++ app/routers/financial_product_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter,Depends
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
-from app.services.financial_product_service import get_deposit_products, get_saving_products
+from app.services.financial_product_service import get_deposit_products, get_saving_products, get_insurance_products
 
 router = APIRouter(prefix="/financial_products", tags=["Financial Products"])
 
@@ -13,3 +13,7 @@ def read_deposit_products(db: Session = Depends(get_db)):
 @router.get("/savings")
 def read_saving_products(db: Session = Depends(get_db)):
     return get_saving_products(db)
+
+@router.get("/insuances")
+def read_insurance_products(db: Session = Depends(get_db)):
+    return get_insurance_products(db)
--- app/services/financial_product_service.py
+++ app/services/financial_product_service.py
@@ -1,8 +1,11 @@
 from sqlalchemy.orm import Session
-from app.models.financial_product_model import DepositProduct, SavingProduct
+from app.models.financial_product_model import DepositProduct, SavingProduct, InsuranceProduct
 
 def get_deposit_products(db: Session):
     return db.query(DepositProduct).all()
 
 def get_saving_products(db: Session):
     return db.query(SavingProduct).all()
+
+def get_insurance_products(db: Session):
+    return db.query(InsuranceProduct).all()
