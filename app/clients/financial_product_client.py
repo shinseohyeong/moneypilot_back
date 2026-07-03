100fbe0b7f31609779de70a9b74f7c4f63dd869a
daeun <daeun3929@gmail.com>
2026-07-03 15:22:41 +0900

    feat: 금융감독원 API 호출 기능 추가

---
+++ app/clients/financial_product_client.py
@@ -0,0 +1,42 @@
+import requests
+from app.core.config import settings
+
+DEPOSIT_API_URL = "http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json"
+SAVING_API_URL = "http://finlife.fss.or.kr/finlifeapi/savingProductsSearch.json"
+
+def fetch_deposit_products():
+    params = {
+        "auth": settings.FINANCE_API_KEY,
+        "topFinGrpNo": "020000",
+        "pageNo": "1",
+        "financeCd": "0010587",
+    }
+
+    # API 호출
+    response = requests.get(
+    DEPOSIT_API_URL,
+    params=params,
+    )
+    
+    # 에러 확인
+    response.raise_for_status()
+
+    # JSON으로 반환
+    return response.json()
+
+def fetch_saving_products():
+    params = {
+        "auth": settings.FINANCE_API_KEY,
+        "topFinGrpNo": "020000",
+        "pageNo": 1,
+        "financeCd": "0010587",
+    }
+
+    response = requests.get(
+        SAVING_API_URL,
+        params=params,
+    )
+
+    response.raise_for_status()
+
+    return response.json()
