fdce4f10344024c16136e8e7dcfb9317287cb8b0
daeun <daeun3929@gmail.com>
2026-07-10 15:11:10 +0900

    feat: repository 분리

--- app/services/financial_product_service.py
+++ app/services/financial_product_service.py
@@ -1,108 +1,9 @@
 from sqlalchemy.orm import Session, selectinload
 from app.models.financial_product_model import DepositProduct, DepositProductRate, SavingProduct, SavingProductRate, InsuranceProduct
 from app.clients.financial_product_client import fetch_deposit_products, fetch_saving_products
-
-def get_deposit_products(
-        db: Session,
-        bank: str | None = None,
-        term: int | None = None,
-        sort: str | None = None,
-        ):
-    query = (
-        db.query(DepositProduct)
-        .options(selectinload(DepositProduct.rates))
-    )
-    
-    # 은행명으로 조회
-    if bank:
-        query = query.filter(
-            DepositProduct.bank_name == bank
-        )
-
-    products = query.all()
-
-    # 가입기간으로 조회
-    if term:
-        result = []
-
-        for product in products:
-            product.rates = [
-                rate for rate in product.rates
-                if rate.term_months == term
-            ]
-
-            if product.rates:
-                result.append(product)
-
-        products = result
-    
-    if sort == "max_rate":
-        products.sort(
-            key=lambda product: max(rate.max_rate for rate in product.rates),
-            reverse=True
-        )
-    elif sort == "base_rate":
-        products.sort(
-            key=lambda product: max(rate.base_rate for rate in product.rates),
-            reverse=True
-        )
-    
-    return products
-
-
-def get_saving_products(
-        db: Session,
-        bank: str | None = None,
-        term: int | None = None,
-        sort: str | None = None,
-        ):
-    
-    query = (
-        db.query(SavingProduct)
-        .options(selectinload(SavingProduct.rates))
-    )
-    
-    # 은행명으로 조회
-    if bank:
-        query = query.filter(
-            SavingProduct.bank_name == bank
-        )
-
-    products = query.all()
-
-    # 가입기간으로 조회
-    if term:
-        result = []
-
-        for product in products:
-            product.rates = [
-                rate for rate in product.rates
-                if rate.term_months == term
-            ]
-
-            if product.rates:
-                result.append(product)
-
-        products = result
-    
-    if sort == "max_rate":
-        products.sort(
-            key=lambda product: max(rate.max_rate for rate in product.rates),
-            reverse=True
-        )
-    elif sort == "base_rate":
-        products.sort(
-            key=lambda product: max(rate.base_rate for rate in product.rates),
-            reverse=True
-        )
-    
-    return products
-
-def get_insurance_products(db: Session):
-    return (
-        db.query(InsuranceProduct)
-        .all()
-    )
+from app.repositories.financial_product_repository import (
+    get_deposit_products, get_saving_products
+)
 
 
 def sync_deposit_products(db: Session):
@@ -248,10 +149,11 @@ def recommend_deposit_products(
     limit: int = 5,
     preferred_bank: str | None = None
 ):
-    products = (
-        db.query(DepositProduct)
-        .options(selectinload(DepositProduct.rates))
-        .all()
+    
+    products = get_deposit_products(
+        db=db,
+        bank=preferred_bank,
+        term=term,
     )
 
     recommend_list = []
@@ -325,10 +227,10 @@ def recommend_saving_products(
     limit: int = 5,
     preferred_bank: str | None = None
 ):
-    products = (
-        db.query(SavingProduct)
-        .options(selectinload(SavingProduct.rates))
-        .all()
+    products = get_saving_products(
+        db=db,
+        bank=preferred_bank,
+        term=term,
     )
 
     recommend_list = []
